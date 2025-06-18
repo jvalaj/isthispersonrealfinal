1e1f22c4544373a1adf9e8c3f46e5eb60e31d52f
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2025-06-18 14:06:57 -0400

    dasd

--- src/App.tsx
+++ src/App.tsx
@@ -260,13 +260,13 @@ function App() {
           </section>
 
           {/* Name Search Section */}
-          <div className="mb-12 w-full sm:w-3/4">
-            <h2 className="text-xl font-bold mb-4">Enter This Person's Name</h2>
+          <div className="mb-12 w-full sm:w-1/2">
+            <h2 className="text-xl font-bold mb-4">Enter Full Name</h2>
             <div className="border border-gray-700 bg-gray-900 rounded-lg p-8">
               <input
                 type="text"
-                placeholder="e.g. John Doe"
-                className="w-full px-4 py-2 rounded bg-black border border-gray-700 text-white mb-4 focus:outline-none focus:border-blue-400"
+                placeholder="disabled for now"
+                className="w-full px-4 py-2 rounded bg-black border border-gray-700 text-white mb-4 focus:outline-none focus:border-red-400"
                 disabled
               />
               <p className="text-gray-300 text-sm">
